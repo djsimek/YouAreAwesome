6b98fac14aa09859e5ff9fd86fc8831dd1c604e8
Dan Simek <djsimek@yahoo.com>
2025-07-29 22:46:55 -0500

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Dan Simek on 7/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+    
+            Text("You Are Awesome!")
+                .font(.largeTitle)
+                .foregroundStyle(.orange)
+                .fontWeight(.heavy)
+        }
+        
+        .padding()
+        
+    }
+    
+}
+
+#Preview {
+    ContentView()
+}
