74ff771cc8f3adec6863ee2855971445f706d11f
Dan Simek <djsimek@yahoo.com>
2025-08-01 23:15:47 -0500

    Ch 1.9 Arrays

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,34 +11,54 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
-
+    @State private var messageIndex = 0
+    let messages = ["You Are Awesome!",
+                    "You Are Great!",
+                    "You are Fantastic!",
+                    "Fabulous? That's You!",
+                    "You Make Me Smile",
+                    "When the Genius Bar Needs Help, They Call You!"]
+    
     var body: some View {
-
+        
         VStack {
             Spacer()
-
+            
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-
-
+            
+            
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
-
+                .multilineTextAlignment(.center)
+            
             Spacer()
-
+            
             Button("Press Me!") {
-                let awesomeString = "You Are Awesome!"
-                let greatString = "You Are Great!"
                 
-                message = (message == awesomeString) ? greatString : awesomeString
-                //TODO: Update the imageName variable
-               
-               // imageName = "image" + String(imageNumber)
+                
+                message = messages[messageIndex]
+                //                if (messageIndex < messages.count-1){
+                //                    messageIndex+=1}
+                //                else{
+                //                    messageIndex = 0
+                //                }
+                messageIndex += 1
+                if(messageIndex == messages.count){
+                    messageIndex = 0
+                }
+                
+                
+                
+                
+                //message = (message == message1) ? message2 : message1
+                
+                // imageName = "image" + String(imageNumber)
                 imageName = "image\(imageNumber)"
                 
                 imageNumber += 1
@@ -46,15 +66,15 @@ struct ContentView: View {
                 if imageNumber > 9 {
                     imageNumber = 0
                 }
-
+                
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-           
+            
         }
-
+        
         .padding()
-
+        
     }
 }
 #Preview {
@@ -64,3 +84,4 @@ struct ContentView: View {
 //command a, control i
 //remove extra blank lines:
 //Find > Find and Replace, Replace ^\s*$, Select Regular Expression, leave with blank, all
+// command / -> comments highlighted code
