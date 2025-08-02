4328d2f781ac37e856b5803afb2faf8910157692
Dan Simek <djsimek@yahoo.com>
2025-08-01 23:31:45 -0500

    Ch 1.10 .minimumScaleFacotr and Swift UI animation modifier

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -13,33 +13,38 @@ struct ContentView: View {
     @State private var imageNumber = 0
     @State private var messageIndex = 0
     let messages = ["You Are Awesome!",
+                    "Gadzooks my friend!  I am astonished at how utterly magnificent you are! jkhlkjh  jh jkh ljljhlh lkjh jkhlhjkhjh lkjh l jljh 6876876b 876897689b 8769b89b89768976b 87 786987698   akjdflk ;kl ;lkjaf lkjas;d;llkj a;flk  klj ;l a;lkdf j;    kl;adsfj kladfjl;kafj ;lakdsfj af   aklsdfj ;alkjfd ;afkads;f  la;kdfj ",
+                    "When the Genius Bar Needs Help, They Call You!",
                     "You Are Great!",
                     "You are Fantastic!",
                     "Fabulous? That's You!",
-                    "You Make Me Smile",
-                    "When the Genius Bar Needs Help, They Call You!"]
+                    "You Make Me Smile"]
     
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
+                //.animation(.bouncy(duration: 0.5, extraBounce: 0.2), value: imageName)
+                .animation(.default, value: imageName)
             
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
+
             
             Spacer()
             
-            Button("Press Me!") {
+            Button("Show Message") {
                 
                 
                 message = messages[messageIndex]
